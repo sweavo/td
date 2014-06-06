a773ffda2ab51d99d49957439020a3fe9562913e
Steve Carter <sweavo@gmail.com>
2014-06-06 17:49:12 +0100

    Tests (such as they are) pass

--- test/test.sh
+++ test/test.sh
@@ -2,11 +2,12 @@
 
 
 # Test script for td
-TEMP=.tmp.$$
+TEMP_ERR=.err.$$.tmp
+TEMP_OUT=.out.$$.tmp
 
 function test_td
 {
-  $TD "$@" 2>$TEMP
+  $TD -f ./test.pickle "$@" 2>$TEMP_ERR >$TEMP_OUT
 }
 
 ## test subject
@@ -17,18 +18,16 @@ TD=../td
 
 ### Check that -f works. If not, we mustn't continue as we might scrub user data
 echo -n "Test 1: " 
-if test_td -f ./minus-f-check.pickle "ls" | fgrep -q minus-f-check-ok
+if $TD -f ./minus-f-check.pickle "ls" 2>/dev/null | fgrep -q minus-f-check-ok
 then
 	echo "Pass."
 else
 	echo "FAIL. Stop."
 	exit 1
 fi
-cat $TEMP 1>&2
 
 ### Remove (if present) the test's pickle file
 rm -f test.pickle
-TD="$TD -f ./test.pickle"
 
 
 ## Can we add an item and does it return an ID?
@@ -36,7 +35,8 @@ TD="$TD -f ./test.pickle"
 ### on stdout.
 ### check that td accepts command line in a single argv.
 echo -n "Test 2: "
-ID1=$(test_td "add this is my first test item")
+test_td "add this is my first test item" 
+read ID1 < $TEMP_OUT
 if [ "$ID1" == "this" ]
 then
   echo "Pass."
@@ -44,17 +44,18 @@ else
   echo "$ID1"
   echo "FAIL."
 fi
-cat $TEMP 1>&2
+cat $TEMP_ERR 1>&2
 
 ## If we add a second item does the ID bump up?
 ### also check that td accepts multiple spaces between verb and operand.
 echo -n "Test 3: "
-ID2=$(test_td "add  this is my second test item")
+test_td "add  this is my second test item"
+read ID2 < $TEMP_OUT
 if [ "$ID2" == "thi1" ]
 then
   echo "Pass."
 else
   echo "FAIL."
 fi
-cat $TEMP 1>&2
+cat $TEMP_ERR 1>&2
 
